7436d0230b7e303fcf46c4129fca9699c7ef1714
Anox3D <71902913+Anox3D@users.noreply.github.com>
2021-03-16 21:40:30 +0100

    Added the (somewhat) original idea of this projec, screenshots and videos

---
+++ (kind of) Original idea/cubepp.cpp
@@ -0,0 +1,78 @@
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <gl/gl.h>
+
+void display()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	// Cube coordinates
+	static GLfloat coords[]=
+	{
+		-0.5, -0.5, -0.5,
+		0.5, -0.5, -0.5,
+		0.5, 0.5, -0.5,
+		-0.5, 0.5, -0.5,
+		-0.5, -0.5, 0.5,
+		0.5, -0.5, 0.5,
+		0.5, 0.5, 0.5,
+		-0.5, 0.5, 0.5
+	};
+	
+	// Here's where the faces are stored, every vertex owns a number
+	static GLubyte num[6][4]=
+	{
+		{ 0, 1, 2, 3 },
+		{ 1, 5, 6, 2 },
+		{ 4, 5, 6, 7 },
+		{ 0, 4, 7, 3 },
+		{ 0, 1, 5, 4 },
+		{ 2, 6, 7, 3 }
+	};
+	
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, coords);
+	glRotatef(0.025, 0.0127, 0.025, 0.025);	// Cube rotation
+	
+	// Draw cube
+	for(int x=0; x!=6; x++)
+	{
+		glBegin(GL_QUADS);
+		for(int i=0; i!=4; ++i)
+		glArrayElement(num[x][i]);
+		glEnd();
+	}
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch(key)
+	{
+		case 27 :		// Press Esc to quit
+			exit(0);
+	}
+}
+
+void glInit(void)
+{
+	glEnable(GL_DEPTH_TEST);	// Activate depth
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glBegin(GL_LINES);
+}
+
+int main(int argc, char **argv)
+{
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInit(&argc, argv);
+	glutInitWindowSize(360, 360);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Cube++");
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glInit();
+	glutMainLoop();
+	return 0;
+}
